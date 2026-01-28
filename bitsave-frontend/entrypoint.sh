dfb02044f2a3635f6728faa11ae714677a52d9f5
pk <phil.kaehler@gmx.de>
2026-01-28 01:35:40 +0100

    feat: implement runtime CSP injection

--- bitsave-frontend/entrypoint.sh
+++ bitsave-frontend/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+export API_BASE_URL=$(echo $API_URL | sed 's/\/api$//')
+
 envsubst '${API_URL} ${BITSAVE_API_KEY} ${SHOW_DEMO}' \
   < /usr/share/nginx/html/assets/config-template.json \
   > /usr/share/nginx/html/assets/config.json
+
+envsubst '${API_BASE_URL}' \
+  < /usr/share/nginx/html/index.html \
+  > /usr/share/nginx/html/index.tmp && mv /usr/share/nginx/html/index.tmp /usr/share/nginx/html/index.html
 
 exec nginx -g "daemon off;"
